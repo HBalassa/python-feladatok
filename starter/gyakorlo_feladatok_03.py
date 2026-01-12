0d4c15edb468bf05aa137935458eb254087c134c
Balassa Hodi <hodi.balassa@gmail.com>
2026-01-12 17:27:25 +0100

    gyakorlas03 starter added

---
+++ starter/gyakorlo_feladatok_03.py
@@ -0,0 +1,55 @@
+# GYAKORLÁS 03
+# 2025.01.12.
+
+###########################
+# 1
+# Google Classroom, Dig. Kult. 11.A/MAT, 2024.10.17. órai munka
+###########################
+# Egy hőmérsékletösszesítő szoftver fejlesztője vagy.
+
+# A program a következő feladatokat oldja meg.
+# 1. Kérj be a felhasználótól 5 hőmérsékletet (celsiusban) és tárold el egy listában.
+# 2. Mekkora  volt a hőingadozás? (maximális és minimális hőmérsékletek különbsége)
+# 3. Hány adat volt az átlaghőmérséklet alatt?
+# 4. Írd ki az összes értéket fahreinheitben!  F = (C × 9/5) + 32
+# 5. Fagyott-e?
+# 6. Melyik volt az első 10 C feletti a hőmérséklet?
+
+
+
+
+###########################
+# 2
+# Google Classroom, Dig. Kult. 11.A/MAT, 2024.10.22. órai munka
+###########################
+# Egy nap jegyzed az ételfogyasztásod, és írsz erre egy programot.
+
+# 1. A program addig kérje be a kalóriamennyiségeket, amíg az összmennyiség el nem éri a 2000 kalóriát.
+# 2. Mennyi volt az átlagos kalóriafogyasztásod?
+# 3. Ma ettél egy Big Mac-et, a nap legmagasabb kalóriatartalmú tételét.  Hány kalóriát tartalmazott?
+# 4. Mikor fogyasztottál először 500 kalóriánál többet egyetlen étkezés során? Mekkora volt az elfogyasztott mennyiség?
+# 5. Hány étkezés után érted el az 1000 összkalóriát?
+
+
+
+
+###########################
+# 3
+# Google Classroom, Dig. Kult. 11.A/MAT, 2024.10.24. órai munka
+###########################
+# Egy Föld körüli pályán keringő űrállomásnak te vagy a kapitánya.
+# Keringésed alatt három erőforrással kell számolnod: üzemanyag, oxigén és energia.
+# Az állomáson 3 feladat elvégzésére van lehetőséged:
+#   1. Űrséta: Ennek végrehajtása 5 üzemanyagot, 50 oxigént és 40 energiát fogyaszt.
+#   2. Karbantartás: Ennek végrehajtása 0 üzemanyagot, 20 oxigént és 20 energiát fogyaszt.
+#   3. Turbó: Ennek végrehajtása 50 üzemanyagot, 10 oxigént és 5 energiát fogyaszt.
+# Kezdetben 200 egység üzemanyaggal, 250 egység oxigénnel és 225 egység energiával indulsz.
+
+# Az állomás számítógépe minden órában megkérdezi, hogy milyen feladatot szeretnél végrehajtani.
+# Ha valamelyik erőforrás nulla alá csökken, a program hagyja abba a kérdések feltevését és közölje velünk:
+# „Figyelmeztetés! A/Az [erőforrás neve] fogyóban! Újratöltés szükséges!”.
+
+# Majd oldjuk meg a következő feladatokat is:
+# 1. Hány feladatot hajtottunk végre összesen?
+# 2. Melyik feladatot választottad a leggyakrabban?
+# 3. Mennyi volt az átlagos üzemanyag-fogyasztásod?
