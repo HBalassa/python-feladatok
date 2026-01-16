38069c66bace2563d456940bdaefd329435f081c
Balassa Hodi <hodi.balassa@gmail.com>
2026-01-16 21:30:43 +0100

    gyak04 updated

---
+++ starter/gyakorlo_feladatok_04.py
@@ -0,0 +1,87 @@
+# GYAKORLÁS 04
+# 2025.01.14.
+
+###########################
+# 1
+# Google Classroom, Dig. Kult. 11.A/MAT, 2024.11.07. órai munka
+###########################
+# Partit rendezel az osztálytársaidnak. A feladatod az, hogy összállítsd a költségvetést,
+# és hogy megtervezd meg, hogy mire költöd a pénzt. Ahhoz, hogy ezt könnyebben megcsináld, írsz egy programot!
+# A program először is bekéri a felhasználótól a buli költségvetését!
+
+# Az így meghatározott pénzt 4 féle dologra költheted:
+#   1) Party sapkák (5 font),
+#   2) Italok (természetesen alkoholmentes, 10 font),
+#   3) dekoráció (5 font),
+#   4) rágcsálnivalók (4 font).
+# A program kérdezze meg a felhasználót, hogy mire szeretné költeni a pénzét, egészen addig, amíg el nem fogy a pénze.
+
+# Majd oldd meg a következő feladatokat!
+# 1. Túlköltekeztél? Ha igen, mennyivel?
+# 2. Mutasd meg a vásárolt összeget minden kategóriában!
+# 3. A teljes költség hány százalékát költötted a partysapkákra?
+# 4. Az összes kategóriából vásároltál termékeket?
+
+
+
+
+###########################
+# 2
+# Google Classroom, Dig. Kult. 11.A/MAT, 2024.11.12. dolgozat "A"
+###########################
+# Egy könyvtárat vezetsz, és nyomon kell követned a könyvkölcsönzéseket. Minden könyvnek a kategóriája alapján kölcsönzési díja van.
+# Állítsd be a költségvetést: Kérdezd meg a felhasználót, hogy mekkora költségvetéssel rendelkezik.
+
+# A könyveknek négy kategóriája van, mindegyikhez kölcsönzési díj tartozik, ezek a következőek:
+#   1) Szépirodalom: 3 dollár könyvenként
+#   2) Nem szépirodalom: 5 dollár könyvenként
+#   3) Gyermekkönyvek: 2 dollár könyvenként
+#   4) Képregény: 1 dollár könyvenként
+# A program kérdezze meg a felhasználótm hogy milyen kategóriát szeretne kölcsözöni, egészen addig,
+# amíg el nem fogy a keret, vagy el nem éri a 10 könyvet.
+# Minden egyes alkalommal, amikor kiválaszt egy kategóriát, vond le a megfelelő összeget a költségvetéséből.
+
+# Ezután oldd meg az alábbi feladatokat!
+# 1. A felhasználó megpróbált a költségvetésén felül kölcsönözni? Ha igen, mennyivel?
+# 2. Mutasd meg, hogy az egyes kategóriákból hány könyvet kölcsönöztek ki.
+# 3. Számítsd ki, hogy a teljes költségvetés hány százalékát költötték szépirodalmi könyvekre.
+# 4. Az összes kategóriából kölcsönzött a felhasználó?
+
+
+
+
+###########################
+# 3
+# Google Classroom, Dig. Kult. 11.A/MAT, 2024.11.12. dolgozat "B"
+###########################
+# Egy programot készítesz a kockadobások nyomon követésére.
+# A programnak először meg kell kérdeznie a felhasználót, hogy hányszor akar kockával dobni.
+# Ezután kérje a felhasználót, hogy adja meg az egyes dobások értékét.
+# A program csak 1 és 6 közötti értékeket fogadjon el; minden ezen a tartományon kívüli dobást 0-nak kell számolni.
+
+# Feladatok:
+# 1. Több páros vagy páratlan számot dobott a felhasználó (a 0-kat nem számítva)?
+# 2. Mennyi volt a dobások átlagértéke?
+# 3. Az összes dobás hány százalékát tették ki az egyes számok (1-től 6-ig)?
+# 4. Hány dobás után dobott a felhasználó először 3-ast?
+# 5. Hány gurítás után érte el az összeg a 10-et?
+
+
+
+
+###########################
+# 4
+# Google Classroom, Dig. Kult. 11.A/MAT, 2024.11.12. dolgozat "C"
+###########################
+# A nap folyamán nyomon követed a vízfogyasztásod.
+# A programnak minden alkalommal meg kell adnia az általad megivott vízmennyiséget ml-ben,
+# amíg a teljes vízmennyiség el nem éri a legalább 3000 ml-t.
+
+# Feladatok:
+# 1. Számítsd ki a bevitelenként átlagosan elfogyasztott vízmennyiséget.
+# 2. Tegyük fel, hogy valamikor megittál egy nagy palack vizet, ami a legmagasabb vízbevitel a nap folyamán.
+#    Hány millilitert tartalmazott ez a palack?
+# 3. Mikor fogyasztottál először 700 ml-nél többet egy alkalommal? Mekkora volt az elfogyasztott mennyiség?
+# 4. Hány bejegyzés után érte el a teljes vízfogyasztásod az 1500 ml-t?
+# 5. Kis italnak számít minden 250 ml alatti, nagy italnak minden 500 ml feletti ital.
+#    Milyen típusú italból ivott a felhasználó a több alkalommal?
