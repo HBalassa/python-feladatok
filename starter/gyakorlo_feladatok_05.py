7593e135b4b10e0dc3c1610234e71abb289380a0
Balassa Hodi <hodi.balassa@gmail.com>
2026-01-22 17:17:31 +0100

    gyak05 uploaded

---
+++ starter/gyakorlo_feladatok_05.py
@@ -0,0 +1,8 @@
+# Képzelj el egy erdőt, amely különböző fákat tartalmaz. Az alábbi fák szerepelnek az erdőben:
+# "tölgy", "nyár", "fenyő", "hárs", "tölgy", "akác", "nyár", "fenyő", "hárs", "akác", "szilfa", "tölgy", "nyár"
+
+# 1. Tárold el a fákat egy megfelelő adatszerkezetben!
+# 2. Írd ki a hány fa található az erdőben!
+# 3. Kérd be a felhasználótól egy fának a nevét! Ez a fa szerepel a listában? Ha igen hanyadik helyen található az első ilyen fa?
+# 4. Melyik fa szerepel a legtöbbször a listában?
+# 5. Szeretnénk egy új fajta fát ültetni az erdőbe. Ehhez a program addig kérje be egy fának a nevét ameddig az nem szerepel az ültetett fák között.
