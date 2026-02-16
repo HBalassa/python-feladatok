033f68b8446c2769dcd0573dab30422f9619163e
BalassaHodi <hodi.balassa@edu.bme.hu>
2026-02-16 11:17:06 +0100

    emelt feladat 01 added

---
+++ starter/emelt_feladat_01/heroes.py
@@ -0,0 +1,19 @@
+###############################################################################################
+# EMELT FELADAT 01
+# 2025.02.16.
+# Google Classroom, Digitális Kultúra Fakultáció 2024-25,  2024.09.03. programozási feladat 1
+###############################################################################################
+
+# A heroes.txt egy videojátékhőseiről tartalmaz adatokat. Egy sora a fájlnak a következő felépítésű: név, szerep,
+# szint, életerő(hp). Ezek az adatok a soron belül szóközökkel vannak elválasztva. A szöveges fájl beolvasásával oldd
+# meg a következő feladatokat:
+
+# 1. Mennyi hős adatait tartalmazza a fájl?
+# 2. Kérd be egy hős nevét, majd jelenítsd meg a hozzátartozó adatokat!
+# 3. Ki a legnagyobb szintű hős? (Megj.: ha több van, elég egyet megjeleníteni!)
+# 4. Mennyi 11-es szintű hős szerepel a fájlban?
+# 5. Szerepenként melyik hősnek van a legnagyobb életereje?
+# 6. Melyik szerephez tarotozik a legtöbb hős?
+# 7. Szintenként mennyi az áltag életerő? (Megj.: Ügyelj arra, hogy csak olyan szintek jelenjenek meg, amelyek
+#    szerepelnek a fájlban!)
+# 8. Írd ki a "warriors.txt" fájlba az összes Warrior szerepű hős nevét és szintjét szint szerint növekvő sorrendben!
